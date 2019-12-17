fd0e0e565416c61f00de97d2b09d1c19a27d43e4
彬辉 李 <2533621490@qq.com>
2019-12-17 19:44:41 +0800

    444

---
+++ 小组项目/MVC/Matchmaker/Controllers/PerInfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Matchmaker.Controllers
+{
+    public class PerInfoController : Controller
+    {
+        // GET: PerInfo
+        public ActionResult Perinfo()
+        {
+            return View();
+        }
+    }
+}
