40ac1cc7091b90c8598c79261a830442a9e3f851
DAIDAIKU <383628213@qq.com>
2019-12-18 11:07:09 +0800

    1

---
+++ 小组项目/MVC/EF/loginDal.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EF
+{
+   public class loginDal
+    {
+        public List<loginUser> GetloginUser() {
+          
+                Model1 db = new Model1();
+                return db.loginUser.ToList();
+            
+        }
+    }
+}
---
+++ 小组项目/MVC/Matchmaker/Controllers/GetListController.cs
@@ -0,0 +1,23 @@
+﻿using EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Matchmaker.Controllers
+{
+    public class GetListController : Controller
+    {
+        //// GET: GetList
+        //Model1 db = new Model1();*
+        public ActionResult Index()
+        {
+            loginDal dal = new loginDal();
+            List<loginUser> list = dal.GetloginUser();
+            return View(list);
+      
+        }
+    }
+}
