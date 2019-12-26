fcb3ed0735711f52f52c80ddbd8821aee68d7389
wyy <1174264916@qq.com>
2019-12-26 16:38:43 +0800

    男生  女生  表  查询

---
+++ 小组项目/MVC/Matchmaker/Controllers/BOYController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Matchmaker.Controllers
+{
+    public class BOYController : Controller
+    {
+        // GET: BOY
+        public ActionResult boyList()
+        {
+            return View();
+        }
+    }
+}
---
+++ 小组项目/MVC/Matchmaker/Controllers/GirlListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Matchmaker.Controllers
+{
+    public class GirlListController : Controller
+    {
+        // GET: GirlList
+        public ActionResult girlList()
+        {
+            return View();
+        }
+    }
+}
